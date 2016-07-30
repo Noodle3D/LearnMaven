baeb62aa438226fc3d0692c82490425bbded3ce5
Bo Yu <yubo1983@gmail.com>
2016-07-31 00:52:00 +0900

    modify the main class

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -2,7 +2,7 @@
  * Created by YuBo on 7/30/16.
  */
 public class ClassOne {
-    public static void main(){
+    public static void main(String[] args){
         System.out.println("This is a maven hello world.");
     }
 }
